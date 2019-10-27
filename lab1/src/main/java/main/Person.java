0e4b1f37f2ff1fe77303fc4c0948c866d6f0c54c
stas_malikov <stas.malikov75@gmail.com>
2019-10-27 08:37:38 +0300

    закоментил класс Person

--- lab1/src/main/java/main/Person.java
+++ lab1/src/main/java/main/Person.java
@@ -34,79 +34,145 @@ public final class Person {
      * @param birthDay день рождения
      * @param gender пол
      */
-    public Person(final int id, final String fullName, final LocalDate birthDay, final String gender) {
+    public Person(final int id, final String fullName,
+                  final LocalDate birthDay, final String gender) {
         this.id = id;
         this.fullName = fullName;
         this.birthDay = birthDay;
         this.gender = gender;
     }
 
-    public void setId(int id) {
+    /**
+     *
+     * @param id присваемый идентификатор.
+     */
+    public void setId(final int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return идентификатор.
+     */
     public int getId() {
         return id;
     }
 
-    public void setFullName(String fullName) {
+    /**
+     *
+     * @param fullName полное имя.
+     */
+    public void setFullName(final String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     *
+     * @return полное имя.
+     */
     public String getFullName() {
         return fullName;
     }
 
-    public void setBirthDay(LocalDate birthDay) {
+    /**
+     *
+     * @param birthDay дата дня рождения.
+     */
+    public void setBirthDay(final LocalDate birthDay) {
         this.birthDay = birthDay;
     }
 
+    /**
+     *
+     * @return дата дня рождения.
+     */
     public LocalDate getBirthDay() {
         return birthDay;
     }
 
-    public int getAge(){
+    /**
+     *Вычисляет возраст человека на сегодняшний день.
+     * @return количество лет.
+     * Если день рождения определён в будущем том возвращяется значение -1.
+     */
+    public int getAge() {
 
         LocalDate now = LocalDate.now();
 
-        if (now.getYear() < birthDay.getYear() || (now.getYear() >= birthDay.getYear() && now.getDayOfYear() < birthDay.getDayOfYear()))
+        if (now.getYear() < birthDay.getYear()
+                || (now.getYear() >= birthDay.getYear()
+                && now.getDayOfYear() < birthDay.getDayOfYear())) {
             return -1;
+        }
 
-        if(now.getMonthOfYear() >= birthDay.getMonthOfYear() && now.getDayOfMonth() >= birthDay.getDayOfMonth())
+        if (now.getMonthOfYear() >= birthDay.getMonthOfYear()
+                && now.getDayOfMonth() >= birthDay.getDayOfMonth()) {
             return now.getYear() - birthDay.getYear();
+        }
 
-        return now.getYear() - birthDay.getYear() - 1 >=0 ? now.getYear() - birthDay.getYear() - 1 : 0;
+        return now.getYear() - birthDay.getYear() - 1 >= 0
+                ? now.getYear() - birthDay.getYear() - 1 : 0;
     }
 
-    public void setGender(String gender) {
+    /**
+     *
+     * @param gender пол человека.
+     */
+    public void setGender(final String gender) {
         this.gender = gender;
     }
 
+    /**
+     *
+     * @return пол человека.
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     *Сгенерированный метод приведения класса к строке.
+     * @return класс в виде строки.
+     */
     @Override
     public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", fullName='" + fullName + '\'' +
-                ", birthDay=" + birthDay +
-                ", gender='" + gender + '\'' +
-                '}';
+        return "Person{" + "id="
+                + id
+                + ", fullName='"
+                + fullName
+                + '\''
+                + ", birthDay="
+                + birthDay
+                + ", gender='"
+                + gender
+                + '\''
+                + '}';
     }
 
+    /**
+     *Сгенерированный метод сравнения двух экземпляров классов.
+     * @param o обьект сравнения.
+     * @return результат сравнения.
+     */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Person person = (Person) o;
-        return getId() == person.getId() &&
-                Objects.equals(getFullName(), person.getFullName()) &&
-                Objects.equals(getBirthDay(), person.getBirthDay()) &&
-                Objects.equals(getGender(), person.getGender());
+        return getId() == person.getId()
+                && Objects.equals(getFullName(), person.getFullName())
+                && Objects.equals(getBirthDay(), person.getBirthDay())
+                && Objects.equals(getGender(), person.getGender());
     }
 
+    /**
+     * Сгенерированный метод вычисления хэша класса.
+     * @return вычисленный хэш.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getFullName(), getBirthDay(), getGender());
